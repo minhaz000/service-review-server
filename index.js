9c5c49cb0cce033376bbba8c9a5e3088e49e1bfe
minhaz000 <mrmminhaz@gamail.com>
2022-11-09 22:20:28 +0600

    single services can get by id

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config()
 require('colors')
 const port = process.env.PORT || 5000
 const app = express()
 /*=================================================================
-........................MiddleWare 
+                   MiddleWare 
 ==================================================================*/     app.use(cors())
 app.use(express.json())
 /*=================================================================
-........................Data Base
+              Data Base Connection 
 ==================================================================*/
 const url = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASSE_PASSWORD}@cluster0.0ang8.mongodb.net/test`
 
@@ -30,17 +30,28 @@ const services = client.db('assignment-11').collection('services')
 const reviews = client.db('assignment-11').collection('reviews')
 dataBaseInit()
 /*=================================================================
-........................Route 
+                         Route 
 ==================================================================*/ 
 
 app.get('/',(req ,res)=>{ 
   res.send(" Service Review server is running !!! ")
  })
 app.get('/services', async(req,res)=>{
-    const  q     = parseInt(req.query.q) 
-    const data   = services.find({}).limit(q)
-    const result = await data.toArray()
-    res.send(result)
+  
+    const lim = parseInt(req.query.l) 
+    const q   = req.query.q
+    if(q){ 
+      const data = services.find({_id:ObjectId(q)})
+      const result = await data.toArray() 
+      res.send(result)
+    }
+    else{
+      const data   = services.find({}).limit(lim)
+      const result = await data.toArray() 
+      res.send(result)
+    }
+
+    
 
  })
  app.post('/services', async(req,res)=>{
