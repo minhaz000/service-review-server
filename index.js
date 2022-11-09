75871e5ed6320106f635afc105f8378d9c6bc904
minhaz000 <mrmminhaz@gamail.com>
2022-11-09 18:26:56 +0600

    services can create and get

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
 require('dotenv').config()
 require('colors')
 const port = process.env.PORT || 5000
 const app = express()
 /*=================================================================
 ........................MiddleWare 
-==================================================================*/                
-app.use(cors())
-
+==================================================================*/     app.use(cors())
+app.use(express.json())
 /*=================================================================
 ........................Data Base
 ==================================================================*/
+const url = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASSE_PASSWORD}@cluster0.0ang8.mongodb.net/test`
+
+const client = new MongoClient(url)
+async function dataBaseInit(){
+  try {
+    await  client.connect()
+    console.log("DateBase Connected Sucessfully !!!".cyan.bold)
+    
+  } catch (error) {
+    console.log(error.massage.red.bold)
+  }
+
+}
+// collections 
+const services = client.db('assignment-11').collection('services')
+const reviews = client.db('assignment-11').collection('reviews')
+dataBaseInit()
 /*=================================================================
 ........................Route 
 ==================================================================*/ 
+
 app.get('/',(req ,res)=>{ 
-  res.send("Photogaraphy server is running !!! ")
+  res.send(" Service Review server is running !!! ")
+ })
+app.get('/services', async(req,res)=>{
+   const   q = parseInt(req.query.q) 
+    const data = services.find({}).limit(q)
+    const result = await data.toArray()
+    res.send(result)
+ })
+ app.post('/services', async(req,res)=>{
+    serveiceObj = req.body.newService 
+    const  result = await services.insertOne(serveiceObj)
+    res.status(201).send({ message:"Service Created Successfull !" , docId:result.insertedId})
  })
 
 /*=================================================================
